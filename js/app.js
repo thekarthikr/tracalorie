6fcea7f9313dd9ba6b654cd033793251cf46e5ab
Karthik <karthikwebdev@outlook.com>
2023-03-14 15:27:27 +0530

    Remove meals and workouts

--- js/app.js
+++ js/app.js
@@ -31,7 +31,28 @@ class trackCalorie{
     }
 
 
-
+    removeMeal(id) {
+        const index = this._meals.findIndex((meal) => meal.id === id);
+    
+        if (index !== -1) {
+          const meal = this._meals[index];
+          this._totalCalories -= meal.calories;
+          this._meals.splice(index, 1);
+          this._render();
+        }
+      }
+    
+      removeWorkout(id) {
+        const index = this._workouts.findIndex((workout) => workout.id === id);
+    
+        if (index !== -1) {
+          const workout = this._workouts[index];
+          this._totalCalories += workout.calories;    
+          this._workouts.splice(index, 1);
+          this._render();
+        }
+      }
+    
 //Private Methods
 
 _displayCaloriesTotal(){
@@ -174,6 +195,8 @@ class App{
 
         document.getElementById('workout-form').
         addEventListener('submit',this._newItem.bind(this,'workout'))
+
+        document.getElementById('meal-items').addEventListener('click',this._removeItem.bind(this,'meal'))
     }
 
 
@@ -203,6 +226,18 @@ _newItem(type,e){
    });
 }  
  
+_removeItem(type,e){
+if(e.target.classList.contains('delete')|| e.target.classList.contains('fa-xmark')){
+  if(confirm('Are you sure want to delete ?')){
+    const id = e.target.closest('.card').getAttribute('data-id');
+
+    type ==='meal' ? this._tracker.removeMeal(id):this._tracker.removeWorkout(id);
+    e.target.closest('.card').remove()
+  }
+}
+  
+}
+
 
 }
 
